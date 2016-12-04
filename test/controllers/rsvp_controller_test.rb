9aa66ec9c983d89f1815278d9a6305cfd428474f
Derek Fons <derek@farfromrest.com>
2016-12-03 20:28:23 -0600

    fix broken test

--- test/controllers/rsvp_controller_test.rb
+++ test/controllers/rsvp_controller_test.rb
@@ -2,12 +2,7 @@ require 'test_helper'
 
 class RsvpControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get rsvp_index_url
-    assert_response :success
-  end
-
-  test "should get success" do
-    get rsvp_success_url
+    get rsvp_url
     assert_response :success
   end
 

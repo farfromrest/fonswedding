073e0bcb3eec06bfda7337a9bf5aa3b610dfa3c2
Derek Fons <derek@farfromrest.com>
2016-12-05 10:55:27 -0600

    clean up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'details', to: 'welcome#details', as: 'details'
-  get 'preview', to: 'welcome#preview'
+  get 'preview', to: 'welcome#preview', as: 'welcome'
   get 'rsvp', to: 'rsvp#index', as: 'rsvp'
   get 'rsvp/confirm/:token', to: 'rsvp#confirm', as: 'confirm'
   post 'rsvp/confirm/:token', to: 'rsvp#confirm', as: 'confirm_submit'
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'welcome#index', as: 'welcome'
+  root to: 'welcome#index'
 end
